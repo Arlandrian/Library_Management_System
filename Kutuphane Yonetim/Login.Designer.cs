8ad915661cabb5b33dad619491b08e4c85556a56
Arlandrian <alo_istanbul@hotmail.com>
2018-04-27 20:10:31 +0300

    Start
    Login page only design

---
+++ Kutuphane Yonetim/Login.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Kutuphane_Yonetim {
+    partial class Login {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.textboxUsername = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.textboxPassword = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.pictureBoxBG = new System.Windows.Forms.PictureBox();
+            this.loginButton = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.labelTitle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textboxUsername
+            // 
+            this.textboxUsername.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textboxUsername.Depth = 0;
+            this.textboxUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.textboxUsername.Hint = "Username";
+            this.textboxUsername.Location = new System.Drawing.Point(130, 155);
+            this.textboxUsername.MouseState = MaterialSkin.MouseState.HOVER;
+            this.textboxUsername.Name = "textboxUsername";
+            this.textboxUsername.PasswordChar = '\0';
+            this.textboxUsername.SelectedText = "";
+            this.textboxUsername.SelectionLength = 0;
+            this.textboxUsername.SelectionStart = 0;
+            this.textboxUsername.Size = new System.Drawing.Size(235, 23);
+            this.textboxUsername.TabIndex = 0;
+            this.textboxUsername.UseSystemPasswordChar = false;
+            // 
+            // textboxPassword
+            // 
+            this.textboxPassword.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textboxPassword.Depth = 0;
+            this.textboxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.textboxPassword.Hint = "Password";
+            this.textboxPassword.Location = new System.Drawing.Point(130, 210);
+            this.textboxPassword.MouseState = MaterialSkin.MouseState.HOVER;
+            this.textboxPassword.Name = "textboxPassword";
+            this.textboxPassword.PasswordChar = '*';
+            this.textboxPassword.SelectedText = "";
+            this.textboxPassword.SelectionLength = 0;
+            this.textboxPassword.SelectionStart = 0;
+            this.textboxPassword.Size = new System.Drawing.Size(235, 23);
+            this.textboxPassword.TabIndex = 1;
+            this.textboxPassword.UseSystemPasswordChar = false;
+            this.textboxPassword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textboxPassword_KeyDown);
+            // 
+            // pictureBoxBG
+            // 
+            this.pictureBoxBG.Location = new System.Drawing.Point(-46, 63);
+            this.pictureBoxBG.Name = "pictureBoxBG";
+            this.pictureBoxBG.Size = new System.Drawing.Size(583, 425);
+            this.pictureBoxBG.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxBG.TabIndex = 4;
+            this.pictureBoxBG.TabStop = false;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Depth = 0;
+            this.loginButton.Location = new System.Drawing.Point(210, 271);
+            this.loginButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Primary = true;
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 2;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click_1);
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(58, 96);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(378, 35);
+            this.labelTitle.TabIndex = 3;
+            this.labelTitle.Text = "Library Management System";
+            this.labelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Login
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(494, 435);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.textboxPassword);
+            this.Controls.Add(this.textboxUsername);
+            this.Controls.Add(this.pictureBoxBG);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.MinimizeBox = false;
+            this.Name = "Login";
+            this.Text = "Login";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Login_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBG)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialSingleLineTextField textboxUsername;
+        private MaterialSkin.Controls.MaterialSingleLineTextField textboxPassword;
+        private System.Windows.Forms.PictureBox pictureBoxBG;
+        private MaterialSkin.Controls.MaterialRaisedButton loginButton;
+        private System.Windows.Forms.Label labelTitle;
+    }
+}
+
---
+++ Kutuphane Yonetim/Login.cs
@@ -0,0 +1,65 @@
+﻿using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace Kutuphane_Yonetim {
+    public partial class Login : MaterialForm {
+        public Login() {
+            InitializeComponent();
+            labelTitle.AutoSize = false;
+            labelTitle.Font = new System.Drawing.Font("Roboto", 18f,FontStyle.Bold);
+
+            SetColor();
+            //SetBackgroundPicture(210,0);
+            //pictureBoxBG.Visible = false;
+        }
+
+        private void loginButton_Click_1(object sender, EventArgs e) {
+            textboxUsername.Text += "k";
+        }
+
+        #region layoutInit
+        private void SetBackgroundPicture(int opacity, int x) {
+            Bitmap pic = new Bitmap(Kutuphane_Yonetim.Properties.Resources.images2);
+            for (int w = 0; w < pic.Width; w++) {
+                for (int h = 0; h < pic.Height; h++) {
+                    Color c = pic.GetPixel(w, h);
+                    Color newC = Color.FromArgb(opacity, c.R + x < 0 ? 0 : c.R + x, c.G + x < 0 ? 0 : c.G + x, c.B + x < 0 ? 0 : c.B + x);
+                    pic.SetPixel(w, h, newC);
+                }
+            }
+            pictureBoxBG.Image = pic;
+        }
+
+        private void SetColor() {
+
+            MaterialSkin.MaterialSkinManager skinManager = MaterialSkin.MaterialSkinManager.Instance;
+            skinManager.AddFormToManage(this);
+            skinManager.ColorScheme = new MaterialSkin.ColorScheme(MaterialSkin.Primary.Amber600, MaterialSkin.Primary.Grey800, MaterialSkin.Primary.Blue700, MaterialSkin.Accent.Cyan400, MaterialSkin.TextShade.WHITE);
+        }
+
+        #endregion
+
+        #region takeEnter
+        private void Login_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter) {
+                loginButton.PerformClick();
+            }
+        }
+
+        private void textboxPassword_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter) {
+                loginButton.PerformClick();
+            }
+        }
+        #endregion
+    }
+}
